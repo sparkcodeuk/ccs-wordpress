4ca0378784ced153b011563fb63e992bca701557
Sean Dunwoody <sean.dunwoody@studio24.net>
2019-02-08 11:22:11 +0000

    Add custom taxonomies

--- public/wp-content/plugins/ccs-custom/library/custom-taxonomies.php
+++ public/wp-content/plugins/ccs-custom/library/custom-taxonomies.php
@@ -2,5 +2,5 @@
 
 add_action( 'init', 'ccs_register_my_taxonomies' );
 function ccs_register_my_taxonomies() {
-    //register_taxonomy( 'taxonomy_name', array( 'post-type' ), array('hierarchical' => true, 'label' => 'label'));
+    //register_taxonomy( 'framework_category', array( 'framework' ), array('hierarchical' => true, 'label' => 'Framework Category'));
 }
